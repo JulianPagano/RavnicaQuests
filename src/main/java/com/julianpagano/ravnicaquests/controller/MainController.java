024513481a8b0a99e2ebc7380665e116d13639f6
Julián Pagano <julianmgs@gmail.com>
2019-02-26 19:53:19 -0300

    Agregado soporte para JSP.

--- src/main/java/com/julianpagano/ravnicaquests/controller/MainController.java
+++ src/main/java/com/julianpagano/ravnicaquests/controller/MainController.java
@@ -3,15 +3,13 @@ package com.julianpagano.ravnicaquests.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	@ResponseBody
-	public String home() {
-		return "Hello World!";
+	public String index() {
+		return "index";
 	}
 
 }
