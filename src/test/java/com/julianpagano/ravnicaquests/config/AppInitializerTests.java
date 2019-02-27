f7a04cbcd9fc595e18b55e5dbe5bd6c876dd4594
Julián Pagano <julianmgs@gmail.com>
2019-02-27 00:04:04 -0300

    Test con nombre correcto.

--- src/test/java/com/julianpagano/ravnicaquests/config/AppInitializerTests.java
+++ src/test/java/com/julianpagano/ravnicaquests/config/AppInitializerTests.java
@@ -1,4 +1,4 @@
-package com.julianpagano.ravnicaquests;
+package com.julianpagano.ravnicaquests.config;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MainApplicationTests {
+public class AppInitializerTests {
 
 	@Test
 	public void contextLoads() {
